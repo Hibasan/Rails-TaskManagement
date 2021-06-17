536cac9360fef35c7bd206f2ee393401c1f7de44
ibaki hongo <redroom013@gmail.com>
2021-06-17 18:03:32 +0900

    Rspecツールの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks ,only: [:new,:create,:edit,:update,:destroy]
+  resources :tasks ,only: [:index,:new,:create,:edit,:update,:destroy]
 end
 
 
