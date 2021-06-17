08d55af06a50b4199c58fa74bb623d5bfbaf7b87
ibaki hongo <redroom013@gmail.com>
2021-06-17 14:24:44 +0900

    タスクテーブルのCRUD機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks ,only: [:new,:create,:edit,:update,:destroy]
 end
+
+
+# root       GET /                             tasks#index
+# tasks     POST /tasks(.:format)              tasks#create
+# new_task   GET /tasks/new(.:format)          tasks#new
+# edit_task  GET /tasks/:id/edit(.:format)     tasks#edit
+#  task    PATCH /tasks/:id(.:format)          tasks#update
+#            PUT /tasks/:id(.:format)          tasks#update
+#         DELETE /tasks/:id(.:format)          tasks#destroy
