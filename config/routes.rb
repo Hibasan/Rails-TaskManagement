0068c3ce81cc93caee6460413594892348a7f0d5
ibaki hongo <redroom013@gmail.com>
2021-06-18 12:14:18 +0900

    SystemSpecのコード完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks ,only: [:index,:new,:create,:edit,:update,:destroy]
+  resources :tasks ,only: [:index,:new,:create,:show,:edit,:update,:destroy]
 end
 
 
-# root       GET /                             tasks#index
-# tasks     POST /tasks(.:format)              tasks#create
-# new_task   GET /tasks/new(.:format)          tasks#new
-# edit_task  GET /tasks/:id/edit(.:format)     tasks#edit
-#  task    PATCH /tasks/:id(.:format)          tasks#update
-#            PUT /tasks/:id(.:format)          tasks#update
-#         DELETE /tasks/:id(.:format)          tasks#destroy
+# root GET    /                                                                                        tasks#index
+#                     tasks GET    /tasks(.:format)                                                                         tasks#index
+#                           POST   /tasks(.:format)                                                                         tasks#create
+#                  new_task GET    /tasks/new(.:format)                                                                     tasks#new
+#                 edit_task GET    /tasks/:id/edit(.:format)                                                                tasks#edit
+#                      task GET    /tasks/:id(.:format)                                                                     tasks#show
+#                           PATCH  /tasks/:id(.:format)                                                                     tasks#update
+#                           PUT    /tasks/:id(.:format)                                                                     tasks#update
+#                           DELETE /tasks/:id(.:format)                                                                     tasks#destroy
