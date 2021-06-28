9c52a11747b038a28d99ee81cea10ee6857cf82d
ibaki hongo <redroom013@gmail.com>
2021-06-28 16:39:36 +0900

    admin権限が無くならないようコールバック機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
   validates :password ,presence: true
   has_many :tasks, dependent: :destroy
   has_secure_password
+
+
+  before_destroy :admin_destroy
+  before_update :admin_update
+
+  private
+  def admin_destroy
+    if User.where(admin:"true").count == 1 && self.admin == true
+        throw :abort
+    end
+  end
+
+  def admin_update
+    if User.where(admin:"true").count == 1 && self.admin == false
+        throw :abort
+    end
+  end
+
 end
